d811abc921ae448696808d5b09cb05a06c3cf9b0
Taynad <166086044+Taynad@users.noreply.github.com>
2024-12-11 10:46:21 -0300

    exercicios 009 e 010 javascript

--- aula11/exer008.js
+++ aula11/exer008.js
@@ -1,6 +1,7 @@
-var vel = 78.5
+var vel = 78.5;
 console.log(`A velocidade do carro é ${vel}Km/h`);
 
 if( vel > 60){
-    console.log(`Você`)
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`);
 }
+console.log(`Dirija sempre com cinto de segurança!`);
