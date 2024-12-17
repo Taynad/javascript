d1f19b4c6a90cf9c07831a52d7670cd19e6ed843
Taynad <166086044+Taynad@users.noreply.github.com>
2024-12-17 10:34:37 -0300

    Create objeto01.js

---
+++ aula17/objeto01.js
@@ -0,0 +1,9 @@
+    //criando obejeto em javascript
+    
+    let amigo = {nome: 'José', 
+        sexo: 'M',
+        engordar(p){
+            console.log('Engordou')
+            this.peso += p;
+        }
+    }
